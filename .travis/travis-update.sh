73272a8ccb1eb85e9f9fc1bff5ffcba48e96a731
Jake Chiang <jakewchiang@gmail.com>
2018-04-26 19:12:05 -0700

    Added data auto-update.

--- .travis/travis-update.sh
+++ .travis/travis-update.sh
@@ -6,21 +6,25 @@ setup_git() {
     git config --global user.name "Travis CI"
 }
 
+update_data() {
+    cd ..
+    NUM_MODIFIED=$(git whatchanged --format=oneline | grep "^:" | grep $FILTER | cut -d " " -f 5 | cut -f 2 | sort | uniq | wc -l)
+    NUM_TOTAL=$(find "$PWD" | grep $FILTER | wc -l)
+    PERCENT=$(echo "scale=5; $NUM_MODIFIED/$NUM_TOTAL" | bc)
+    DATE=$(git show -s --format=%ci)
+    cd verigames.github.io
+    echo "$DATE=$PERCENT" >> data.txt
+}
+
 commit_website_files() {
     git clone https://github.com/verigames/verigames.github.io.git
     cd verigames.github.io
     REPO=`git config remote.origin.url`
     SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
     setup_git
-    ls > index.html
-    git add index.html
+    update_data
+    git add data.txt
     git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
-
-    #ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-    #ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-    #ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-    #ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-    #openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
     chmod 600 ../.travis/deploy_key
     eval `ssh-agent -s`
     ssh-add ../.travis/deploy_key

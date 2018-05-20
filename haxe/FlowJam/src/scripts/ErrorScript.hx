aa73e92ff356cb222b0f2799210788204a1627b7
kristen autumn blackburn <kblack37@uw.edu>
2018-05-20 13:54:23 -0700

    added errorscript example

---
+++ haxe/FlowJam/src/scripts/ErrorScript.hx
@@ -0,0 +1,36 @@
+package scripts;
+
+import constraints.events.ErrorEvent;
+import engine.IGameEngine;
+import engine.scripting.ScriptNode;
+import scenes.game.components.MiniMap;
+
+/**
+ * ...
+ * @author kristen autumn blackburn
+ */
+class ErrorScript extends ScriptNode {
+	
+	private var errorDict : Dynamic;
+	
+	private var m_minimap : MiniMap;
+
+	public function new(gameEngine : IGameEngine, id:String=null) {
+		super(id);
+		
+		errorDict = {};
+		
+		gameEngine.addEventListener(ErrorEvent.ERROR_ADDED, onErrorAdded);
+		gameEngine.addEventListener(ErrorEvent.ERROR_REMOVED, onErrorRemoved);
+	}
+	
+	private function onErrorAdded(evt : ErrorEvent) : Void
+    {
+        Reflect.setField(errorConstraintDict, evt.constraintError.id, evt.constraintError);
+    }
+    
+    private function onErrorRemoved(evt : ErrorEvent) : Void
+    {
+		Reflect.deleteField(errorConstraintDict, evt.constraintError.id);
+    }
+}
